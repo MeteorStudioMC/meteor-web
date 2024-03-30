07f39b4e06d3d069e767115bf6ced6d523413ac8
Anhuar Ruiz <myclxss@outlook.com>
2024-03-30 12:27:25 -0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,23 @@
     onscroll(document, toggleBacktotop)
   }
 
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+  
+
   on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
